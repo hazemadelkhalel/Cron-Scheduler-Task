841b85cdf24f93f3a736df618232a473d4409faf
Hazem Adel <74595174+hazemadelkhalel@users.noreply.github.com>
2024-07-08 14:35:45 +0300

    Initial commit: Set up Cron Scheduler project structure

---
+++ TeldaBankTask/src/main/java/com/example/TeldaBankTask/TeldaBankTaskApplication.java
@@ -0,0 +1,11 @@
+package com.example.TeldaBankTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeldaBankTaskApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TeldaBankTaskApplication.class, args);
+    }
+}
